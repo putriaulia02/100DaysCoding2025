f26fc912cbe5e2725dbe96f0cc1d49d6b2d425a8
putriaulia02 <putriaulia211221@gmail.com>
2025-09-09 16:37:00 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,12 @@
+package putri;
+
+public class day2 {
+    public static void main(String[] args){
+        String nama = "putri aulia";
+        int umur = 18;
+        double tinggi = 154.0;
+        System.out.printf("nama saya %s saya berumur %d dan tinggi saya itu %.1f \n" ,nama,umur,tinggi);
+        System.out.println("nama : " + nama);
+        System.out.print("umur : " + umur);
+    }
+}
