aba6b3cef0a31ff99b1c23d69e43110f05e34416
putriaulia02 <putriaulia211221@gmail.com>
2025-09-10 15:22:25 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,18 @@
+package day3.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String name = "Putri aulia";
+        /*yang diatas adalah tipe data string 
+        dengan variavel name dan berisi putri aulia*/
+        /* untuk komentar yang memiliki tanda bintang dan garis miring(/*)
+        dan ditutup lagi dengan bintang dan garis miring ini/ 
+        yaitu untuk komentar banyak baris atau bnyak line*/
+         
+        System.out.println(name);
+        //yang diatas untuk manampilkan hasil setelah di run/ di output
+        //dan komen yang memiliki 2 garis miring atau 2 slice untuk komen 1 line/baris
+    }
+    
+}
