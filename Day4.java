8ae3de3a2cb15262fe6ad746dfa0042a9127bc86
putriaulia02 <putriaulia211221@gmail.com>
2025-09-11 16:43:17 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,25 @@
+package day4.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "putri aulia";
+        System.out.println("nama \t:" + nama);
+        String nim = "D0225024";
+        System.out.println("nim \t:" + nim);
+        //fungsi backslash t(\t) untuk memberi jarak
+        int umur = 18;
+        System.out.println("u\bumur :" + umur + " tahun");
+        //backlslash b(\b) untuk menghapus karakter sebelumnya
+        char kelas = 'c';
+        System.out.println("kelas \n:" + kelas + " informatika");
+        //fungsi backslash n(\n) untuk membuat baris baru 
+        System.out.println("kabar saya it\'s ok");
+        //fungsi backslash '(\') untuk menambah tanda koma atas pada kata yang kita tulis
+        System.out.println("saya sekolah di UPTD SMAN 1 kalukku\\SMANSAKAL");
+        //fungsi backslash \\(\\) untuk memberi atau menampikan tanda backslash
+        System.out.println("putri : \"bagaimana kabar kamu?\"");
+        //fungsi backslash "(\") untuk menambah atau menampilkan tanda kutip
+    }
+    
+}
