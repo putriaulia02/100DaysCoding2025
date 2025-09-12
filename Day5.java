e823664b5da248d76c6f7c39b774b9d166c68b90
putriaulia02 <putriaulia211221@gmail.com>
2025-09-12 15:44:23 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,18 @@
+package day5.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        /*tipe data numerik bilangan bulat terdiri dari 4 yaitu
+         int, long, byte, short*/
+        int umur = 18;
+        long jumlah_tmnkls = 29;
+        byte saudara = 3;
+        short jumlah_jaritangan = 10;
+        System.out.println("umur : " + umur + " thn");
+        System.out.println("jumlah teman kelas : " + jumlah_tmnkls + " cm");
+        System.out.println("jumlah saudara : " + saudara);
+        System.out.println("jumlah jari tangan : " + jumlah_jaritangan);
+    }
+    
+}
