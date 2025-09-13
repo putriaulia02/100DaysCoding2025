7f65999a6e498045e76d0930586019c674b9b71a
putriaulia02 <putriaulia211221@gmail.com>
2025-09-13 12:55:16 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,17 @@
+package day6.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // tipe data untuk pecahan yaitu ada double dan float
+        double tinggi = 154.0;
+        float bb = 44.0f;
+        System.out.println("Tinggi : " + tinggi);
+        System.out.println("Berat badan : " + bb);
+        /* perbedaan tipe data double dan float
+        float : digunakan untuk angka desimal namun kapasitas lebih kecil
+        double: digunakan untuk angka desimal jg namun dgn kapasitas lebij besar
+         */
+    }
+    
+}
