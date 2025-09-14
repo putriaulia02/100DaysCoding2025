3dc0ef64caac2a9d95685be12bec389bc2f30b0c
putriaulia02 <putriaulia211221@gmail.com>
2025-09-14 11:09:18 +0800

    Day47.java

---
+++ Day47.java
@@ -0,0 +1,14 @@
+package day7.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        char kelas = 'c';
+        boolean sehat = true;
+        System.out.println("kelas : " + kelas + " informatika");
+        System.out.println("sehat : " + sehat);
+        //tipe data char untuk menyimpan satu karakter saja dan memakai tanda kutip 1(')
+        //tipe data boolean untuk menyimpan 2 kemungkinan yaitu true(benar) dan false(salah)
+    }
+    
+}
