e3f4cbddcf4494a334b36aa407c59b5e7d1e9c10
putriaulia02 <putriaulia211221@gmail.com>
2025-09-15 16:09:07 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,19 @@
+package day8.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "putri aulia";
+        String alamat = "galung";
+        String prodi = "informatika";
+        System.out.println("Nama : " + nama);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Prodi : " + prodi);
+        //tipe data String digunakan untuk teks, kalimat atau kata
+        //tipe data string juga memakai tanda kutip dua(")
+        /*tipe data string juga bisa berisi angka,simbol,dan huruf 
+        selama dianggap teks dan menggunakan tanda kutip dua(")
+         */
+    }
+    
+}
