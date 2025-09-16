3d564b755078556c8da77f2bc379c169b1b9deac
putriaulia02 <putriaulia211221@gmail.com>
2025-09-16 20:41:50 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,24 @@
+package day9.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //deklarasi adalah proses membuat tipe data dan variabel
+        int umur;
+        
+        /*inisialisasi adalah proses memberi nilai kepada
+        variabel setelah deklarasi*/
+        umur = 17;
+        
+        //menampilkan nilai awal
+        System.out.println("umur thn 2024 : " + umur);
+        
+        /*update variabel adalah proses mengubah nilai 
+        variabel pertama dengan nilai yang baru */
+        umur = 18;
+        
+        //menampilkan nilai setelah di update variabel
+        System.out.println("umur thn 2025 : " + umur);
+    }
+    
+}
