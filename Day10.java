2ca7732c9a562253e6de66ad563a9113f7437be7
putriaulia02 <putriaulia211221@gmail.com>
2025-09-17 12:40:25 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,14 @@
+package day10.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        final int jumlahSaudara = 3;
+        System.out.println("Jumlah saudara sekarang & seterusnya : " + jumlahSaudara);
+        /*constanta (Final variabel) adalah nilai yang yang tidak bisa
+        diganti atau nilai yang dari awal akan tetap dan tidak bisa diperbarui
+        atau di update
+         */
+    }
+    
+}
