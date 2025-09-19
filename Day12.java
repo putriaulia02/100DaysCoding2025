6d91b559f22ce6674f1f2d47d233b6605f8be5b9
putriaulia02 <putriaulia211221@gmail.com>
2025-09-19 22:10:45 +0800

    Day12.java

--- Day12.java
+++ Day12.java
@@ -3,31 +3,52 @@ package day12.java;
 public class Main {
     
     public static void main(String[] args) {
-        /* hari ini saya membuat biodata saya dengan menggunakan
+        /* hari ini saya membuat biodata saya dengan menggunakan scanner dan
          beberapa tipe data yaitu string, int, double, float, dan char*/
         String nama, alamat, nim, asalSekolah;
         int umur;
         double tinggi;
         float bb;
         char kelas;
-        nama = "Putri aulia";
-        alamat = "kalukku (galung)";
-        nim = "D0225024";
-        asalSekolah = "UPTD SMAN 1 Kalukku";
-        umur = 17;
-        tinggi = 154.0;
-        bb = 44.0f;
-        kelas = 'C';
-        //perintah untuk mencetak di output
+        //penggunaan scanner
+        System.out.print("Nama :");
+        nama = sc.nextLine();
+        System.out.print("Alamat :");
+        alamat = sc.nextLine();
+        System.out.print("NIM :");
+        nim = sc.nextLine();
+        System.out.print("Asal Sekolah :");
+        asalSekolah = sc.nextLine();
+        System.out.print("Umur :");
+        umur = sc.nextInt();
+        System.out.print("Tinggi badan :");
+        tinggi = sc.nextDouble();
+        System.out.print("Berat badan :");
+        bb = sc.nextFloat();
+        System.out.print("Kelas :");
+        kelas = sc.next().charAt(0);
+
+        //yang akan mencetak di output
+        System.out.println("===============");
         System.out.println("==Biodata==");
         System.out.println("Nama :" + nama);
         System.out.println("Alamat : " + alamat);
         System.out.println("NIM :" + nim);
         System.out.println("Asal Sekolah :" + asalSekolah);
         System.out.println("Umur :" + umur + "thn");
-        System.out.println("Tinggi badan :" + tinggi + "cm");
-        System.out.println("Berat badan :" + bb + "kg");
-        System.out.println("Kelas :" + kelas + "Informatika");
+        System.out.println("Tinggi badan :" + tinggi + " cm");
+        System.out.println("Berat badan :" + bb + " kg");
+        System.out.println("Kelas :" + kelas + " Informatika");
+
+        /*penjelasan penggunaan scanner untuk membaca inputan dari tipe data 
+         yang digunakan(string, int, double, float, dan char)*/
+        /*string => next.Line(); mencetak/membaca semua kata yang dimasukkan
+                 => next(); hanya mencetak/membaca satu kata atau hanya kata pertama
+          int => next.Int();
+          double => next.Double();
+          float => next.Float();
+          char => next().charAt(0); *(0) bisa diganti sesuai dengan inisal 
+          yang kita mau dari nilai variabel*/
     }
     
 }
