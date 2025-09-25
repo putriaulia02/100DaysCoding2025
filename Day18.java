8d428545c0ddb2e6f98b123049284441c9074eda
putriaulia02 <putriaulia211221@gmail.com>
2025-09-25 21:50:34 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,18 @@
+package day18;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //konversi otomatis(dari tipe data kecil ke besar)
+        //byte ke int
+        byte a = 18;
+        long b = a;
+        System.out.println("byta ke long : " + b);
+        
+        //float ke double
+        int c = 44;
+        double d = c;
+        System.out.println("float ke double : " + d);   
+    }
+    
+}
