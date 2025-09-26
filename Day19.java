9e71bf47dccf2fdaf1cb1727057a3b66fe1cf0e6
putriaulia02 <putriaulia211221@gmail.com>
2025-09-26 20:59:06 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,13 @@
+package day19;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //konversi manual/paksa(dari tipe data besar ke kecil)
+        //float ke byte
+        float x = 44.0f;
+        byte y = (byte) x;
+        System.out.println("float ke byte " + y);
+    }
+    
+}
