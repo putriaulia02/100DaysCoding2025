38d4ecaf108897d38db9eb2747fd556adb008b76
putriaulia02 <putriaulia211221@gmail.com>
2025-09-27 20:07:22 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,16 @@
+package day20;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Mengubah tipe data primitif menjadi string
+        int a = 100;
+        String nilai = String.valueOf(a);
+        System.out.println("int ke String :" + a);
+        
+        double b = 154.0;
+        String tinggi = b + "";
+        System.out.println("double ke String :" + b);
+    }
+    
+}
