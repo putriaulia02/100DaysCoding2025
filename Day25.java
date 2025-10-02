336dece4caa0bb7a8e09f0f26c1f67aa6142ca1a
putriaulia02 <putriaulia211221@gmail.com>
2025-10-02 20:26:09 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,20 @@
+package day25;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //nilai awal variabel a dan b
+        int a = 8;
+        int b = 6;
+        //nila a dan b setelah di increment dan dicrement
+        a++;  //post increment
+        System.out.println("nilai a di post increment: " + a);
+        ++a;  //pre increment
+        System.out.println("nilai a di pre increment: " + a);
+        b--;  //post dicrement
+        System.out.println("nilai b di post decrement: " + b);
+        --b;  //pre dicrement
+        System.out.println("nilai b di pre dicrement: " + b);
+    }
+    
+}
