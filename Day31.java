37cd4c3e68ac570b198c8032acaedc9d05991ce4
putriaulia02 <putriaulia211221@gmail.com>
2025-10-08 19:26:54 +0800

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,17 @@
+package day31;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Operator perbandingan AND(&&)
+        System.out.print("Tinggi: ");
+        double tinggi = input.nextDouble();
+        System.out.print("Nilai rata rata: ");
+        int nilai = input.nextInt();
+        
+        boolean lulus = (tinggi >= 156) && (nilai >= 80);
+        System.out.println("Apakah saya lulus paskib tingkat kabupaten? : " + lulus);
+    }
+    
+}
