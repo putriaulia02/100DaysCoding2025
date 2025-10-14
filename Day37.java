a2a2bb7b0a0d6feef4dac1cb0e5101b005157fa4
putriaulia02 <putriaulia211221@gmail.com>
2025-10-14 19:32:37 +0800

    Day37.java

---
+++ Day37.java
@@ -0,0 +1,16 @@
+package day37;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("angka: ");
+        int angka = sc.nextInt();
+        if(angka % 2 == 0){
+            System.out.println("Genap");
+        }else{
+            System.out.println("Ganjil");
+        }
+    }
+    
+}
