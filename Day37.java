5a02dc9d58f1d41702f5b01b1ec97a81624ab948
putriaulia02 <putriaulia211221@gmail.com>
2025-10-14 21:17:29 +0800

    Day37.java

--- Day37.java
+++ Day37.java
@@ -6,10 +6,34 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("angka: ");
         int angka = sc.nextInt();
-        if(angka % 2 == 0){
-            System.out.println("Genap");
-        }else{
-            System.out.println("Ganjil");
+        if(angka <= 9){
+            System.out.print(angka + " Termasuk bilangan satuan");
+            if(angka % 2 == 0){
+                System.out.println(" dan genap");
+            }else{
+                System.out.println(" dan ganjil");
+            }
+        }else if(angka <= 99){
+            System.out.print(angka + " Termasuk bilangan puluhan");
+            if(angka % 2 == 0){
+                System.out.println(" dan genap");
+            }else{
+                System.out.println(" dan ganjil");
+                }
+        }else if(angka <= 999){
+            System.out.print(angka + " Termasuk bilangan ratusan");
+            if(angka % 2 == 0){
+                System.out.println(" dan genap");
+            }else{
+                System.out.println(" dan ganjil");
+                }
+        }else if(angka <= 9999){
+            System.out.print(angka + " Termasuk bilangan ribuan");
+            if(angka % 2 == 0){
+                System.out.println(" dan genap");
+            }else{
+                System.out.println(" dan ganjil");
+                }
         }
     }
     
