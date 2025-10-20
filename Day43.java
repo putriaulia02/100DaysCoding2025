3f13634e64c8bba47e97950b7797dbafb4e81566
putriaulia02 <putriaulia211221@gmail.com>
2025-10-20 23:43:55 +0800

    Day43.java

--- Day43.java
+++ Day43.java
@@ -11,41 +11,39 @@ public class Main {
         String ganjilOrgenap = "";
         String hasilbagi = "";
         if(angka > 0){
-        //menentukan bilangannya termasuk bilangan apa
-        if(angka > 0 && angka <= 9){
+            if(angka > 0 && angka <= 9){
             bilangan = " angka adalah satuan";
-        }else if(angka > 0 && angka <= 99){
+            }else if(angka > 0 && angka <= 99){
             bilangan = " angka adalah puluhan";
-        }else if(angka > 0 && angka <= 999){
+            }else if(angka > 0 && angka <= 999){
             bilangan = " angka adalah ratusan";
-        }else{
+            }else{
             //bilangan = " yang diinputkan tidak boleh mines";
-        }
+            }
         
-        //menentukan ganjil genap
-        if(angka > 0 && angka % 2 == 0){
-            ganjilOrgenap = " genap";
-        }else if(angka > 0 && angka % 2 == 1){
-            ganjilOrgenap = " ganjil";
-        }else{
+            //menentukan ganjil genap
+            if(angka > 0 && angka % 2 == 0){
+                 ganjilOrgenap = " genap";
+            }else if(angka > 0 && angka % 2 == 1){
+                 ganjilOrgenap = " ganjil";
+            }else{
             
-        }
+            }
         
-        //menentukan hasil yang bisa dibagi 3,5, dan 3 dan 5
-        if(angka > 0 && (angka % 3 == 0 && angka % 5 == 0)){
-            hasilbagi = " yang bisa dibagi 3 dan 5";
-        }else if(angka > 0 && angka % 3 == 0){
-            hasilbagi = " yang bisa dibagi 3";
-        }else if(angka > 0 && angka % 5 == 0){
-            hasilbagi = " yang bisa dibagi 5";
+            //menentukan hasil yang bisa dibagi 3,5, dan 3 dan 5
+            if(angka > 0 && (angka % 3 == 0 && angka % 5 == 0)){
+                hasilbagi = " yang bisa dibagi 3 dan 5"; 
+            }else if(angka > 0 && angka % 3 == 0){
+                hasilbagi = " yang bisa dibagi 3";
+            }else if(angka > 0 && angka % 5 == 0){
+                hasilbagi = " yang bisa dibagi 5";
+            }else{
+                hasilbagi = " tidak habis dibagi 3 dan 5";
+            }
+            //mencetak keseluruhan
+            System.out.println(angka + bilangan + ganjilOrgenap + hasilbagi);
         }else{
-            System.out.println("angka yang diinputkan tidak boleh negatif"); 
-        }
-        
-        //mencetak keseluruhan
-        System.out.println(angka + bilangan + ganjilOrgenap + hasilbagi);
-        }else{
-            System.out.println("tidak menerima inputan negatif");
+             System.out.println("tidak menerima inputan negatif");
         }    
     }
     
