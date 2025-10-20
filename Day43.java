6a4a9084def868ea3f8c219bcf345f590ef64cf2
putriaulia02 <putriaulia211221@gmail.com>
2025-10-20 19:45:39 +0800

    Day43.java

--- Day43.java
+++ Day43.java
@@ -6,46 +6,38 @@ public class Main {
         Scanner p = new Scanner(System.in);
         System.out.print("Masukkan angka: ");
         int angka = p.nextInt();
-        if(angka <= 9){
-            System.out.print(angka + " adalah angka satuan");
-            if(angka % 2 == 0){
-                System.out.print(" genap");
-                if(angka % 3 == 0){
-                    System.out.println(" yang bisa dibagi 3");
-                }
-            }else{
-                System.out.println(" ganjil");
-                if(angka % 5 == 0){
-                    System.out.println(" yang bisa dibagi 5");
-                }
-            }
-        }else if(angka <= 99){
-            System.out.print(angka + " adalah angka puluhan");
-            if(angka % 2 == 0){
-                System.out.print(" genap");
-                if(angka % 3 == 0 && angka % 5 == 0){
-                    System.out.println(" yang bisa dibagi 3 dan 5");
-                }
-            }else{
-                System.out.print(" ganjil");
-                if(angka % 3 == 0 && angka % 5 == 0){
-                    System.out.println(" yang bisa dibagi 3 dan 5");
-                }
-            }
-        }else if(angka <= 999){
-            System.out.print(angka + " adalah angka ratusan");
-            if(angka % 2 == 0){
-                System.out.print(" genap");
-                if(angka % 3 == 0 && angka % 5 == 0){
-                    System.out.println(" yang bisa dibagi 3 dan 5");
-                }
-            }else{
-                System.out.print(" ganjil");
-                if(angka % 3 == 0 && angka % 5 == 0){
-                    System.out.println(" yang bisa dibagi 3 dan 5");
-                }
-            }
+        
+        String bilangan = "";
+        String ganjilOrgenap = "";
+        String hasilbagi = "";
+        
+        //menentukan bilangannya termasuk bilangan apa
+        if(angka < 9){
+            bilangan = " satuan";
+        }else if(angka < 99){
+            bilangan = " puluhan";
+        }else if(angka < 999){
+            bilangan = " ratusa";
         }
+        
+        //menentukan ganjil genap
+        if(angka % 2 == 0){
+            ganjilOrgenap = " genap";
+        }else{
+            ganjilOrgenap = " ganjil";
+        }
+        
+        //menentukan hasil yang bisa dibagi 3,5, dan 3 dan 5
+        if(angka % 3 == 0 && angka % 5 == 0){
+            hasilbagi = " yang bisa dibagi 3 dan 5";
+        }else if(angka % 3 == 0){
+            hasilbagi = " yang bisa dibagi 3";
+        }else if(angka % 5 == 0){
+            hasilbagi = " yang bisa dibagi 5";
+        }
+        
+        //mencetak keseluruhan
+        System.out.println(angka + " adalah angka " + bilangan + ganjilOrgenap + hasilbagi);
     }
     
 }
