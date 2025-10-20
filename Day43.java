00469c5d48011fa986bd6c85cc25183cf9e0f32e
putriaulia02 <putriaulia211221@gmail.com>
2025-10-20 23:26:54 +0800

    Day43.java

--- Day43.java
+++ Day43.java
@@ -10,36 +10,43 @@ public class Main {
         String bilangan = "";
         String ganjilOrgenap = "";
         String hasilbagi = "";
-        
+        if(angka > 0){
         //menentukan bilangannya termasuk bilangan apa
-        if(angka <= 9){
-            bilangan = " satuan";
-        }else if(angka <= 99){
-            bilangan = " puluhan";
-        }else if(angka <= 999){
-            bilangan = " ratusa";
+        if(angka > 0 && angka <= 9){
+            bilangan = " angka adalah satuan";
+        }else if(angka > 0 && angka <= 99){
+            bilangan = " angka adalah puluhan";
+        }else if(angka > 0 && angka <= 999){
+            bilangan = " angka adalah ratusan";
         }else{
-            hasilbagi = " angka tidak habis dibagi 3 dan 5";
+            //bilangan = " yang diinputkan tidak boleh mines";
         }
         
         //menentukan ganjil genap
-        if(angka % 2 == 0){
+        if(angka > 0 && angka % 2 == 0){
             ganjilOrgenap = " genap";
-        }else{
+        }else if(angka > 0 && angka % 2 == 1){
             ganjilOrgenap = " ganjil";
+        }else{
+            
         }
         
         //menentukan hasil yang bisa dibagi 3,5, dan 3 dan 5
-        if(angka % 3 == 0 && angka % 5 == 0){
+        if(angka > 0 && (angka % 3 == 0 && angka % 5 == 0)){
             hasilbagi = " yang bisa dibagi 3 dan 5";
-        }else if(angka % 3 == 0){
+        }else if(angka > 0 && angka % 3 == 0){
             hasilbagi = " yang bisa dibagi 3";
-        }else if(angka % 5 == 0){
+        }else if(angka > 0 && angka % 5 == 0){
             hasilbagi = " yang bisa dibagi 5";
+        }else{
+            System.out.println("angka yang diinputkan tidak boleh negatif"); 
         }
         
         //mencetak keseluruhan
-        System.out.println(angka + " adalah angka " + bilangan + ganjilOrgenap + hasilbagi);
+        System.out.println(angka + bilangan + ganjilOrgenap + hasilbagi);
+        }else{
+            System.out.println("tidak menerima inputan negatif");
+        }    
     }
     
 }
