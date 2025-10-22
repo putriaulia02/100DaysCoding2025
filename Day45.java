a94172c8f4898a6357adf2aec7c400d5b29fde85
putriaulia02 <putriaulia211221@gmail.com>
2025-10-22 22:25:54 +0800

    Day45.java

--- Day45.java
+++ Day45.java
@@ -4,24 +4,23 @@ public class Main {
     
     public static void main(String[] args) {
         Scanner p = new Scanner(System.in);
-        System.out.print("Masukkan pilihan kelas (1=A/2=B/3=C/4=D): ");
-        String pilihan = p.nextLine();
+        System.out.print("Masukkan pilihan kelas (1=A,2=B,3=C,4=D: ");
+        int pilihan = p.nextInt();
         switch(pilihan){
-            case "1":
+            case 1:
             System.out.println("Kamu masuk kelas A");
             break;
-            case "2":
+            case 2:
             System.out.println("Kamu masuk kelas B");
             break;
-            case "3":
+            case 3:
             System.out.println("Kamu masuk kelas C");
             break;
-            case "4":
+            case 4:
             System.out.println("Kamu masuk kelas D");
             break;
             default:
             System.out.println("Pilihan kelas tidak ada");
-            break;
         }
     }
     
