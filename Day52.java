a955101344dbaf59b712b370dfd8d25f4d933a4e
putriaulia02 <putriaulia211221@gmail.com>
2025-10-29 18:43:10 +0800

    Day52.java

--- Day52.java
+++ Day52.java
@@ -7,14 +7,19 @@ public class Main {
         while (true) {
             System.out.print("Masukkan angka: ");
             int angka = p.nextInt();
-            if(angka < 10){
-                System.out.println(angka + " adalah bilangan satuan");
-            }else if(angka < 100){
-                System.out.println(angka + " adalah bilangan puluhan");
-            }else if(angka < 1000){
-                System.out.println(angka + " adalah bilangan ratusan");
+            if(angka > 0){
+                if(angka < 10){
+                   System.out.println(angka + " adalah bilangan satuan");
+                }else if(angka < 100){
+                   System.out.println(angka + " adalah bilangan puluhan");
+                }else if(angka < 1000){
+                   System.out.println(angka + " adalah bilangan ratusan");
+                }else{
+                   System.out.println(angka + " adalah bilangan lain(ribuan, puluh ribuan, dan seterusnya)");
+                   break;
+                }
             }else{
-                System.out.println(angka + " adalah bilangan lain(ribuan, puluh ribuan, dan seterusnya)");
+                System.out.println("tidak menerima inputan nol dan negatif");
                 break;
             }
         }
