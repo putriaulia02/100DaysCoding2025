fa0c358e18bfcff525d282b81a70e6b7e529f86a
putriaulia02 <putriaulia211221@gmail.com>
2025-10-29 18:30:05 +0800

    Day52.java

---
+++ Day52.java
@@ -0,0 +1,23 @@
+package day52;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner p = new Scanner(System.in);
+        while (true) {
+            System.out.print("Masukkan angka: ");
+            int angka = p.nextInt();
+            if(angka < 10){
+                System.out.println(angka + " adalah bilangan satuan");
+            }else if(angka < 100){
+                System.out.println(angka + " adalah bilangan puluhan");
+            }else if(angka < 1000){
+                System.out.println(angka + " adalah bilangan ratusan");
+            }else{
+                System.out.println(angka + " adalah bilangan lain(ribuan, puluh ribuan, dan seterusnya)");
+                break;
+            }
+        }
+    }
+    
+}
