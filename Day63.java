d009b0deb720e7bee3bc1d3a70b5b871484e43e7
putriaulia02 <putriaulia211221@gmail.com>
2025-11-09 19:30:38 +0800

    Day63.java

---
+++ Day63.java
@@ -0,0 +1,19 @@
+package day63;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner p = new Scanner(System.in);
+        System.out.print("Masukkan nilai N : ");
+        int N = p.nextInt();
+        int total = 1;
+        
+        for(int i = 1; i <= N; i++){
+            total *=i;
+            System.out.print(i + " x ");
+        }
+        
+        System.out.println("\b\b = " + total);
+    }
+    
+}
