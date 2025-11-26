1227688de29792f166be5726e0f841c860872e1d
putriaulia02 <putriaulia211221@gmail.com>
2025-11-26 20:55:02 +0800

    Day80.java

---
+++ Day80.java
@@ -0,0 +1,13 @@
+package day80;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Array 1D
+        int [] nilai = {89, 90, 98, 78, 100};
+        for (int i = 0; i < nilai.length; i++){
+            System.out.println("index ke-"+i + " : "+ nilai[i] + " ");
+        }
+    }
+    
+}
