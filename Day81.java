214157dcd29888df98ec58c0cc09fb01209b4242
putriaulia02 <putriaulia211221@gmail.com>
2025-11-27 21:58:29 +0800

    Day81.java

---
+++ Day81.java
@@ -0,0 +1,14 @@
+package day81;
+
+public class day81 {
+    public static void main(String[] args) {
+        //latihan akses elemen array dengan indeks
+        int [] nilai = {70, 80, 90, 100};
+        
+        System.out.println("Indeks 0 : " + nilai[0]);
+        System.out.println("Indeks 1 : " + nilai[1]);
+        System.out.println("Indeks 2 : " + nilai[2]);
+        System.out.println("Indeks 3 : " + nilai[3]);
+    }
+ 
+}
