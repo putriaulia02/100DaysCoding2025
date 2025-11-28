94f51b358e8f87bc1cef72ad75c0b99b38c97733
putriaulia02 <putriaulia211221@gmail.com>
2025-11-28 20:50:19 +0800

    Day82.java

---
+++ Day82.java
@@ -0,0 +1,11 @@
+package day82;
+
+public class day82 {
+    public static void main(String[] args) {
+        int [] angka  = {43, 5, 12, 57, 17, 36, 329};
+        
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("angka indeks ke-" +i + " : " + angka[i]);
+        }
+    }
+}
