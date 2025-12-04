13f8779a9a78395b973163a1b123c0ad8ac3ea1f
putriaulia02 <putriaulia211221@gmail.com>
2025-12-04 20:23:36 +0800

    Day88.java

---
+++ Day88.java
@@ -0,0 +1,14 @@
+package day88;
+
+public class day88 {
+    public static void main(String[] args) {
+        int [] nilai = {10, 20, 30, 40, 50, 60};
+        
+        System.out.print("Menampilkan isi array : ");
+        for (int i : nilai) {
+            System.out.print(i+" ");
+        }
+        
+        System.out.println("");
+    }
+}
