5ab92c9a1bbda781e55a75ff18bc775d8e6cddf8
putriaulia02 <putriaulia211221@gmail.com>
2025-12-07 21:04:14 +0800

    Day91.java

---
+++ Day91.java
@@ -0,0 +1,10 @@
+package day91;
+
+public class day91 {
+    public static void main(String[] args) {
+        halo("putri");
+    }
+    static void halo(String nama){
+        System.out.println("Halo saya "+nama);
+    }
+}
