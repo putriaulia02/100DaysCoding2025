cbd8291b78fadc565860ed5a1ca4aa8c19f8a281
putriaulia02 <putriaulia211221@gmail.com>
2025-12-09 21:46:30 +0800

    Day93.java

---
+++ Day93.java
@@ -0,0 +1,20 @@
+package day93;
+
+public class day93 {
+    public static void main(String[] args) {
+        //method  ceil
+        double angka = 45.8;
+        System.out.println(Math.ceil(angka));
+        
+        //method floor
+        double nilai = 98.5;
+        System.out.println(Math.floor(nilai));
+        
+        //method round
+        double bilangan1 = 56.3;
+        System.out.println(Math.round(bilangan1));
+        double bilangan2 = 77.8;
+        System.out.println(Math.round(bilangan2));
+        
+    }
+}
