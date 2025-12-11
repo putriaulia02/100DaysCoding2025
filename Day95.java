fea30a3ca8c54c6ed973ea854340e8669d692735
putriaulia02 <putriaulia211221@gmail.com>
2025-12-11 22:42:34 +0800

    Day95.java

---
+++ Day95.java
@@ -0,0 +1,11 @@
+package day95;
+
+public class day95 {
+    public static void main(String[] args) {
+        
+        namasaya("Putri Aulia");
+    }
+    public static void namasaya(String nama){
+        System.out.println("Nama Lengkap saya adalah : "+nama);
+    }
+}
